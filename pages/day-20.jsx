118c811e61a59208edd45f47da746e3a7443095e
sarincasm <eelsweb@gmail.com>
2020-11-28 22:27:45 +0530

    day-20 wip

--- pages/day-20.jsx
+++ pages/day-20.jsx
@@ -8,8 +8,19 @@ import Frame from '../components/Frame'
 import {Mail, Send} from 'grommet-icons'
 import {useState} from 'react'
 
+const CircleAnimation = keyframes`
+	0% {
+		transform: translate(-50%, -65%) scale(1);
+	}
+	20% {
+		transform: translate(-50%, -65%) scale(1.1);
+	}
+	100% {
+		transform: translate(-50%, -65%) scale(0);
+	}
+`
 const Circle = styled.div`
-	position: relative;
+	position: absolute;
 	top: 50%;
 	left: 50%;
 	transform: translate(-50%, -65%);
@@ -17,11 +28,32 @@ const Circle = styled.div`
 	height: 200px;
 	background: #354a5f;
 	border-radius: 50%;
+	animation: ${({animationState}) =>
+		animationState
+			? css`
+					${CircleAnimation} 1.5s ease-in-out
+			  `
+			: 'initial'};
+	animation-fill-mode: both;
+`
+
+const OuterCircle = styled(Circle)`
+	box-sizing: border-box;
+	background: none;
+	border: 4px solid #354a5f;
+	animation: ${({animationState}) =>
+		animationState
+			? css`
+					${CircleAnimation} 1.2s ease-in-out .3s
+			  `
+			: 'initial'};
+	animation-fill-mode: both;
 `
 
 const IconDiv = styled.div`
 	position: absolute;
 	left: 50%;
+	top: 50%;
 	transform: translate(-50%, -75%);
 	z-index: 2;
 `
@@ -56,7 +88,7 @@ const ButtonAnimation = keyframes`
 		opacity: 1;
 	}
 `
-const Button = styled.button`
+const Button = styled.div`
 	position: absolute;
 	bottom: 15%;
 	left: 50%;
@@ -68,6 +100,7 @@ const Button = styled.button`
 	border-radius: 20px;
 	border-width: 0;
 	text-transform: uppercase;
+	text-align: center;
 	font-size: 15px;
 	cursor: pointer;
 
@@ -96,7 +129,8 @@ export default function Day() {
 	return (
 		<Layout titleFragment="Day Number - Component Title">
 			<Frame>
-				<Circle />
+				<Circle animationState={status !== 'ready'} />
+				<OuterCircle animationState={status !== 'ready'} />
 				<IconDiv>
 					<Icon size="xlarge" color="white" />
 				</IconDiv>
