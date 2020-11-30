6f4a60a50c0272c9935b66dab65f6af185972c53
sarincasm <eelsweb@gmail.com>
2020-12-01 00:05:14 +0530

    day-20

--- pages/day-20.jsx
+++ pages/day-20.jsx
@@ -5,9 +5,9 @@ import styled, {css, keyframes} from 'styled-components'
 import Layout from '../components/Layout'
 import Frame from '../components/Frame'
 
-import {Mail, Send} from 'grommet-icons'
 import {useState} from 'react'
 
+const CircleColor = '#4052DE'
 const CircleAnimation = keyframes`
 	0% {
 		transform: translate(-50%, -65%) scale(1);
@@ -26,7 +26,7 @@ const Circle = styled.div`
 	transform: translate(-50%, -65%);
 	width: 50%;
 	height: 200px;
-	background: #354a5f;
+	background: ${CircleColor};
 	border-radius: 50%;
 	animation: ${({animationState}) =>
 		animationState
@@ -40,7 +40,7 @@ const Circle = styled.div`
 const OuterCircle = styled(Circle)`
 	box-sizing: border-box;
 	background: none;
-	border: 4px solid #354a5f;
+	border: 4px solid ${CircleColor};
 	animation: ${({animationState}) =>
 		animationState
 			? css`
@@ -50,17 +50,80 @@ const OuterCircle = styled(Circle)`
 	animation-fill-mode: both;
 `
 
-const IconDiv = styled.div`
+const IconContainer = styled.svg`
 	position: absolute;
 	left: 50%;
 	top: 50%;
-	transform: translate(-50%, -75%);
+	transform: translate(-60px, -68px);
 	z-index: 2;
+
+	fill: none;
+	stroke-width: 2px;
+	stroke: white;
+	stroke-linecap: square;
+	stroke-dasharray: 325;
+`
+const MailAnimation = keyframes`
+	from {
+		stroke-dashoffset: 0;
+	}
+	to {
+		stroke-dashoffset: 325;
+	}
+`
+const MailIconContainer = styled(IconContainer)`
+	animation: ${({animationState}) =>
+		animationState
+			? css`
+					${MailAnimation} 1.2s ease-in-out .3s
+			  `
+			: 'initial'};
+	animation-fill-mode: forwards;
+`
+const SentAnimation = keyframes`
+	0% {
+		stroke-dashoffset: 325;
+	}
+	40% {
+		stroke-dashoffset: 0;
+	}
+	55% {
+		transform: translate(-60px, -68px) scale(1);
+		stroke-dashoffset: 0;
+	}
+	65% {
+		transform: translate(-70px, -60px) scale(1.05);
+		stroke-dashoffset: 0;
+	}
+	70% {
+		opacity: 1;
+		stroke-dashoffset: 0;
+	}
+	85% {
+		opacity: 0;
+		stroke-dashoffset: 0;
+	}
+	100% {
+		transform: translate(240px, -400px) scale(0.4);
+		opacity: 0;
+	}
+`
+const SentIconContainer = styled(IconContainer)`
+	stroke-dashoffset: 325;
+	animation: ${({animationState}) =>
+		animationState
+			? css`
+					${SentAnimation} 3s ease-in-out 1.4s
+			  `
+			: 'initial'};
+	animation-fill-mode: both;
 `
 
+const ButtonColor = '#DEC262'
+const ButtonComplimentaryColor = 'black'
 const ButtonAnimation = keyframes`
 	10% {
-		background: #1abc9c;
+		background: ${ButtonComplimentaryColor};
 		color: transparent;
 	}
 	30%, 45% {
@@ -94,8 +157,8 @@ const Button = styled.div`
 	left: 50%;
 	transform: translate(-50%);
 	width: 60%;
-	background: #ecf0f1;
-	color: #2c3e50;
+	background: ${ButtonColor};
+	color: ${ButtonComplimentaryColor};
 	line-height: 40px;
 	border-radius: 20px;
 	border-width: 0;
@@ -105,8 +168,8 @@ const Button = styled.div`
 	cursor: pointer;
 
 	&:hover {
-		background: #1abc9c;
-		color: #fff;
+		background: ${ButtonComplimentaryColor};
+		color: ${ButtonColor};
 	}
 	animation: ${({animationState}) =>
 		animationState
@@ -119,7 +182,6 @@ const Button = styled.div`
 export default function Day() {
 	const [status, setStatus] = useState('ready')
 
-	const Icon = status === 'ready' ? Mail : Send
 	const text = {
 		ready: 'Send Email',
 		transition: 'Sent',
@@ -131,9 +193,14 @@ export default function Day() {
 			<Frame>
 				<Circle animationState={status !== 'ready'} />
 				<OuterCircle animationState={status !== 'ready'} />
-				<IconDiv>
-					<Icon size="xlarge" color="white" />
-				</IconDiv>
+				<MailIconContainer animationState={status !== 'ready'}>
+					<polyline points="121,1 121,69 1,69 1,1"></polyline>
+					<polyline points="121,1 61,43 1,1 121,1"></polyline>
+				</MailIconContainer>
+				<SentIconContainer animationState={status !== 'ready'}>
+					<polyline points="119,1 1,59 106,80 119,1"></polyline>
+					<polyline points="119,1 40,67 43,105 69,73"></polyline>
+				</SentIconContainer>
 				<Button
 					animationState={status === 'transition'}
 					onClick={() => {
